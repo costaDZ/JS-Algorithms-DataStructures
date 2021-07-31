f64713fa816bcde5603fec1210bd2cb00b57af57
costaDZ <73201789+costaDZ@users.noreply.github.com>
2021-07-31 15:41:07 +0100

    add dijkstra algo

---
+++ DataStruecture/DijKstra's ALGORITHM/dijkstras's-Algorithm.js
@@ -0,0 +1,28 @@
+
+class WeightedGraph {
+    constructor() {
+        this.adjencencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjencencyList[vertex]) this.adjencencyList[vertex] = [];
+    }
+
+    addEdge(vertex1, vertex2, weight) {
+        this.adjencencyList[vertex1].push({ node: vertex2, weight });
+        this.adjencencyList[vertex2].push({ node: vertex1, weight });
+    }
+}
+
+
+let list = new WeightedGraph();
+
+list.addVertex("A");
+list.addVertex("B");
+list.addVertex("C");
+
+list.addEdge("A", "B", 15)
+list.addEdge("C", "B", 8)
+list.addEdge("A", "C", 20)
+
+console.log(list);
