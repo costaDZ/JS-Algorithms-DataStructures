32d6ea72070a20a53bed77b7901d5aff6ef3f8a1
costaDZ <73201789+costaDZ@users.noreply.github.com>
2021-07-31 15:10:42 +0100

    add dijkstra algo

--- DataStruecture/Graphs/graphsTraversal.js
+++ DataStruecture/Graphs/graphsTraversal.js
@@ -1,137 +1,168 @@
 
-class Graph {
-    constructor() {
-        this.adjecencyList = {};
-    }
-    // test 
+// class Graph {
+//     constructor() {
+//         this.adjecencyList = {};
+//     }
+//     // test 
 
-    addVertex(vertex) {
-        if (!this.adjecencyList[vertex]) {
-            this.adjecencyList[vertex] = [];
-        }
-    }
+//     addVertex(vertex) {
+//         if (!this.adjecencyList[vertex]) {
+//             this.adjecencyList[vertex] = [];
+//         }
+//     }
 
-    addEdge(vertex1, vertex2) {
-        this.adjecencyList[vertex1].push(vertex2);
-        this.adjecencyList[vertex2].push(vertex1);
-    }
+//     addEdge(vertex1, vertex2) {
+//         this.adjecencyList[vertex1].push(vertex2);
+//         this.adjecencyList[vertex2].push(vertex1);
+//     }
 
-    removeEdge(vertex1, vertex2) {
-        this.adjecencyList[vertex1] =
-            this.adjecencyList[vertex1].filter(v => v !== vertex2);
-        this.adjecencyList[vertex2] =
-            this.adjecencyList[vertex2].filter(v => v !== vertex1);
-    }
+//     removeEdge(vertex1, vertex2) {
+//         this.adjecencyList[vertex1] =
+//             this.adjecencyList[vertex1].filter(v => v !== vertex2);
+//         this.adjecencyList[vertex2] =
+//             this.adjecencyList[vertex2].filter(v => v !== vertex1);
+//     }
 
-    removerVertex(vertex) {
-        for (let prop in this.adjecencyList) {
-            this.removeEdge(prop, vertex);
-        }
-        delete this.adjecencyList[vertex];
-    }
+//     removerVertex(vertex) {
+//         for (let prop in this.adjecencyList) {
+//             this.removeEdge(prop, vertex);
+//         }
+//         delete this.adjecencyList[vertex];
+//     }
 
-    depthFirstRecursive1(strVtx) {
-        let list = [];
-        let visited = {};
-        let adjecencyList = this.adjecencyList;
-        (function DFS(Vtx) {
-            if (!Vtx) return null;
-            visited[Vtx] = true;
-            list.push(Vtx);
-            for (let val of adjecencyList[Vtx]) {
-                if (!visited[val]) {
-                    DFS(val);
-                }
-            }
-        })(strVtx);
-        return list;
-    }
+//     depthFirstRecursive1(strVtx) {
+//         let list = [];
+//         let visited = {};
+//         let adjecencyList = this.adjecencyList;
+//         (function DFS(Vtx) {
+//             if (!Vtx) return null;
+//             visited[Vtx] = true;
+//             list.push(Vtx);
+//             for (let val of adjecencyList[Vtx]) {
+//                 if (!visited[val]) {
+//                     DFS(val);
+//                 }
+//             }
+//         })(strVtx);
+//         return list;
+//     }
 
-    depthFirstRecursive(start) {
-        const result = [];
-        const visited = {};
-        const adjacencyList = this.adjecencyList;
+//     depthFirstRecursive(start) {
+//         const result = [];
+//         const visited = {};
+//         const adjacencyList = this.adjecencyList;
 
-        (function dfs(vertex) {
-            if (!vertex) return null;
-            visited[vertex] = true;
-            result.push(vertex);
-            adjacencyList[vertex].forEach(neighbor => {
-                if (!visited[neighbor]) {
-                    return dfs(neighbor)
-                }
-            });
-        })(start);
+//         (function dfs(vertex) {
+//             if (!vertex) return null;
+//             visited[vertex] = true;
+//             result.push(vertex);
+//             adjacencyList[vertex].forEach(neighbor => {
+//                 if (!visited[neighbor]) {
+//                     return dfs(neighbor)
+//                 }
+//             });
+//         })(start);
 
-        return result;
-    }
-
-
-
-    depthFirstIterative(start) {
-        const stack = [start];
-        const result = [];
-        const visited = {};
-        let currentVertex;
-
-        visited[start] = true;
-        while (stack.length) {
-            currentVertex = stack.pop();
-            result.push(currentVertex);
-
-            this.adjecencyList[currentVertex].forEach(neighbor => {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    stack.push(neighbor)
-                }
-            });
-        }
-        return result;
-    }
-}
-
-
-let list = new Graph();
+//         return result;
+//     }
 
 
 
+//     depthFirstIterative(start) {
+//         const stack = [start]; // ===> we use Stack [LIFO]
+//         const result = [];
+//         const visited = {};
+//         let currentVertex;
+
+//         visited[start] = true;
+//         while (stack.length) {
+//             currentVertex = stack.pop();
+//             result.push(currentVertex);
+
+//             this.adjecencyList[currentVertex].forEach(neighbor => {
+//                 // console.log(currentVertex, "////", neighbor, visited, stack);
+//                 if (!visited[neighbor]) {
+//                     visited[neighbor] = true;
+//                     stack.push(neighbor)
+//                 }
+//             });
+//         }
+//         return result;
+//     }
+
+
+//     breadthFirst(start) {
+//         const queue = [start];  // ===> we use QUEUES [FIFO]
+
+//         const result = [];
+//         const visited = {};
+//         let currentVertex;
+
+//         visited[start] = true;
+
+//         while (queue.length) {
+//             currentVertex = queue.shift();
+//             result.push(currentVertex);
+
+//             this.adjecencyList[currentVertex].forEach(neighbor => {
+
+//                 if (!visited[neighbor]) {
+//                     visited[neighbor] = true;
+//                     queue.push(neighbor);
+//                 }
+
+//             })
+//         }
+
+//         return result;
+
+//     }
+// }
+
+
+// let list = new Graph();
 
 
 
-list.addVertex("A")
-list.addVertex("B")
-list.addVertex("C")
-list.addVertex("D")
-list.addVertex("E")
-list.addVertex("F")
-list.addVertex("k")
-list.addVertex("l")
 
 
-list.addEdge("A", "B")
-list.addEdge("A", "C")
-list.addEdge("B", "D")
-list.addEdge("C", "E")
-list.addEdge("D", "E")
-list.addEdge("D", "F")
-list.addEdge("E", "F")
-list.addEdge("E", "k")
-list.addEdge("E", "l")
 
-console.log(list);
-
-console.log(list.depthFirstRecursive1("A"));
-console.log(list.depthFirstRecursive("A"));
-
-console.log(list.depthFirstIterative("A"));
+// list.addVertex("A")
+// list.addVertex("B")
+// list.addVertex("C")
+// list.addVertex("D")
+// list.addVertex("E")
+// list.addVertex("F")
+// list.addVertex("k")
+// list.addVertex("l")
 
 
-console.log(list);
+// list.addEdge("A", "B")
+// list.addEdge("A", "C")
+// list.addEdge("B", "D")
+// list.addEdge("C", "E")
+// list.addEdge("D", "E")
+// list.addEdge("D", "F")
+// list.addEdge("E", "F")
+// // list.addEdge("E", "k")
+// // list.addEdge("E", "l")
 
-//          A
-//        /   \
-//       B     C
-//       |     |
-//       D --- E
-//        \   /
-//          F
+// console.log(list);
+
+// // console.log(list.depthFirstRecursive1("A"));
+// // console.log(list.depthFirstRecursive("A"));
+
+// // console.log(list.depthFirstIterative("A"));
+
+// console.log(list.breadthFirst("A"));
+
+
+// // console.log(list);
+
+// //          A
+// //        /   \
+// //       B     C
+// //       |     |
+// //       D --- E
+// //        \   /
+// //          F
