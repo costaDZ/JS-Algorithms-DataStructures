b4986d6c7268cb7e836bd25fab5c3b693516534e
costaDZ <73201789+costaDZ@users.noreply.github.com>
2021-08-04 16:46:27 +0100

    //

--- Dynamic Programming/dynamicProgramming.js
+++ Dynamic Programming/dynamicProgramming.js
@@ -1,8 +1,13 @@
 
 
-function fibonatci(num) {
-
+function fib(n, memo = []) {
+    //debugger;
+    if (memo[n] !== undefined) return memo[n];
+    if (n <= 2) return 1;
+    let res = fib(n - 1, memo) + fib(n - 2, memo);
+    memo[n] = res;
+    return res;
 }
 
 
-console.log(fibonatci(10));
+console.log(fib(90));
