13e7221772e28cff89bcfdb1d564a18a5ed9d740
costaDZ <73201789+costaDZ@users.noreply.github.com>
2021-08-20 14:36:57 +0100

    BST

--- DataStruecture/binary search tree/binarySearchTree.js
+++ DataStruecture/binary search tree/binarySearchTree.js
@@ -11,34 +11,34 @@
 //         this.root = null;
 //     }
 
-//     insert(val) {
-//         let newNode = new Node(val);
-//         let currentNode = this.root;
+// insert(val) {
+//     let newNode = new Node(val);
+//     let currentNode = this.root;
 
-//         if (!currentNode) {
-//             this.root = newNode;
-//             return this;
-//         } else {
-//             while (true) {
-//                 if (val === currentNode.value) return undefined;
-//                 if (val < currentNode.value) {
-//                     if (currentNode.left) {
-//                         currentNode = currentNode.left;
-//                     } else {
-//                         currentNode.left = newNode;
-//                         return this;
-//                     }
+//     if (!currentNode) {
+//         this.root = newNode;
+//         return this;
+//     } else {
+//         while (true) {
+//             if (val === currentNode.value) return undefined;
+//             if (val < currentNode.value) {
+//                 if (currentNode.left) {
+//                     currentNode = currentNode.left;
 //                 } else {
-//                     if (currentNode.right) {
-//                         currentNode = currentNode.right;
-//                     } else {
-//                         currentNode.right = newNode;
-//                         return this;
-//                     }
+//                     currentNode.left = newNode;
+//                     return this;
+//                 }
+//             } else {
+//                 if (currentNode.right) {
+//                     currentNode = currentNode.right;
+//                 } else {
+//                     currentNode.right = newNode;
+//                     return this;
 //                 }
 //             }
 //         }
 //     }
+// }
 
 //     search(val) {
 //         if (!this.root) return undefined;
@@ -103,20 +103,102 @@
 
 //========================================//
 
-class Node {
-    constructor(val) {
-        this.value = val;
-        this.left = null;
-        this.right = null;
-    }
-}
+// class Node {
+//     constructor(val) {
+//         this.value = val;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
+
+// class BinarySearchTree {
+//     constructor() {
+//         this.root = null;
+//     }
+
+//     insert(val) {
+//         let newNode = new Node(val);
+//         let current = this.root;
+
+//         if (!this.root) return this.root = newNode;
+//         while (true) {
+//             if (current.value === val) return undefined;
+//             if (val > current.value) {
+//                 if (current.right) {
+//                     current = current.right;
+//                 } else {
+//                     current.right = newNode;
+//                     break;
+//                 }
+//             } else {
+//                 if (current.left) {
+//                     current = current.left;
+//                 } else {
+//                     current.left = newNode;
+//                     break;
+//                 }
+//             }
+//         }
+//         return this;
+//     }
+
+//     search(val) {
+//         if (this.root.value === val) return this.root;
+
+//         let current = this.root;
+
+//         while (true) {
+//             if (current.value === val) {
+//                 return current;
+//             } else if (current.value < val) {
+//                 if (!current.right) return undefined;
+//                 current = current.right;
+//             } else {
+//                 if (!current.left) return undefined;
+//                 current = current.left;
+//             }
+//         }
+//     }
 
 
-class BinarySearchTree {
 
-    constructor() {
+//     find(val) {
+//         if (this.root.value === val) return true;
 
-    }
+//         let current = this.root;
+
+//         while (true) {
+//             if (current.value === val) return true;
+
+//             if (current.value > val) {
+//                 if (!current.left) return false;
+//                 current = current.left;
+//             } else {
+//                 if (!current.right) return false;
+//                 current = current.right;
+//             }
+//         }
+//     }
+// }
+
+// let tree = new BinarySearchTree();
+
+// tree.insert(10);
+// tree.insert(8);
+// tree.insert(6);
+// tree.insert(7);
+// // tree.insert(15);
+// // tree.insert(12);
+// // tree.insert(11);
+// // tree.insert(13);
+// // tree.insert(18);
+// // tree.insert(18);
+
+// // console.log(tree.search(10));
+// // console.log(tree.search(15));
+
+// console.log(tree.find(0));
 
 
-}
+
+// console.log(tree);
